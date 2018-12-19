5547c74c384a2bf52156b0463683243707afae06
sdim-ua <ingersol@yandex.ua>
2018-12-19 13:42:02 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,16 @@
+/* 
+Повторенье-мать
+*/
+public class Solution {
+    public static void print3(String s) {
+        //напишите тут ваш код
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+
+    }
+
+    public static void main(String[] args) {
+        print3("I love you!");
+    }
+}
